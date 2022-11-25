d407fcf9db292176555fa889baa469544295f13e
Davit Pirmisashvili <d.pirmisashvili@gmail.com>
2022-11-25 18:00:57 +0400

    Added TransactionRow Added PreviewData defined date formatter helper function

--- Expense Tracker/Extensions.swift
+++ Expense Tracker/Extensions.swift
@@ -8,9 +8,22 @@
 import Foundation
 import SwiftUI
 
+
+// Color choices
 extension Color {
     static let background = Color("Background")
     static let icon = Color("Icon")
     static let text = Color("Text")
     static let systemBackground = Color(uiColor: .systemBackground)
+}
+
+
+// Date Formatter helper function
+extension DateFormatter {
+    static let allNumericUSA: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        
+        return formatter
+    }()
 }
--- Expense Tracker/Preview Content/PreviewData.swift
+++ Expense Tracker/Preview Content/PreviewData.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+import SwiftUI
+
+var transactionPreviewData = Transaction(id: 1, date: "01/24/2022", institution: "TBC", account: "TBC Visa", merchant: "Apple", amount: 249.99, type: "debit", categoryId: 801, category: "Software", isPending: false, isTransfer: false, isExpense: true, isEdited: false)
+
+var transactionPreviewList = [Transaction](repeating: transactionPreviewData, count: 10)
